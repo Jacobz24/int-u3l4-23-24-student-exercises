1731d97e7735caa1d64453275803e51204bf6894
Jacobz24 <146957812+Jacobz24@users.noreply.github.com>
2024-01-17 20:02:50 +0000

    DOM Manipulation

--- domManipulation.js
+++ domManipulation.js
@@ -13,9 +13,13 @@ let image = document.querySelector('.image');
 // CODE ALONG
 // 1. An event listener is provided for the `contentButton`
 // 2. When clicked, it should change the content of:
-//   - The `name` header.
-
-// -- RETURN TO LESSON --
+//  - The `name` header.
+contentButton.addEventListener("click", function() {
+  myName.innerHTML = "Jacob Zuniga";
+  myHobby.innerHTML = "Play Soccer";
+  myLocation.innerHTML = "Brooklyn";
+})
+//-- RETURN TO LESSON --
 
 // CODE SOLO
 //   - The `hobby` paragraph.
@@ -28,11 +32,16 @@ contentButton.addEventListener('click', function() {
 
 
 
+
+
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
+image1Button.addEventListener("mouseover", function() {
+  image.src = "https://frontofficesports.com/wp-content/uploads/2023/10/USATSI_19520555_168393969_lowres-scaled-e1697215176168.jpg?quality=100";
+})
 
 
 
@@ -40,6 +49,9 @@ contentButton.addEventListener('click', function() {
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
 
+image2Button.addEventListener("mouseover", function() {
+  image.src = "https://www.orthodoxhawaii.org/display_image.php?ximgid=paragraph_125_1&ext=jpg&relativeimage=images/sobor.jpg&archive=0&final_h=120&final_w=160&percent=100&m=1297788341";
+})
 
 // BONUS
 // 7. Try using different event types for your buttons.
--- script.js
+++ script.js
@@ -3,16 +3,16 @@ let button = document.querySelector("button");
 let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
-// - What do you think innerHTML does?
-// - What do you think .src does?
+// - What do you think innerHTML does? allows to change text/content
+// - What do you think .src does? allows to change images
 
 
 // 2. Try the following:
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
-    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+    header.innerHTML = "Jacob Zuniga";
+    img.src = "https://media1.tenor.com/m/3aRn8KE-L_QAAAAC/shia-labeouf-christian.gif";
 });
 
 
